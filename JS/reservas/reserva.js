9a634a5ce2f9990c06ade0ef65d28fe287934452
DilsiaLamadridTorres <lamadrid1992@live.com>
2026-09-15 19:03:50 -0500

    corrigiendo css

--- JS/reservas/reserva.js
+++ JS/reservas/reserva.js
@@ -66,10 +66,10 @@ document.addEventListener("DOMContentLoaded", () => {
     let solicitudHorariosActual = 0;
 
     const horariosRespaldo = [
-        "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00",
-        "15:00", "16:00", "17:00", "18:00", "19:00", "20:00"
+        "08:00", "10:00", "11:00", "12:00", "14:00",
+        "15:00", "16:00", "18:00", "19:00", "20:00"
     ];
-    const horariosOcupadosRespaldo = ["09:00", "13:00", "17:00"];
+    const horariosOcupadosRespaldo = [];
 
     function formatearPrecioReserva(valor) {
         const precio = Number(valor);
